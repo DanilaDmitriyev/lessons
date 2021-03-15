3021f6a0f3790fa2d1ff48272bfe9d208889fab9
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-03-15 21:31:47 +0300

    Урок от 15.03.2021

--- LessonMonth/LessonMonth/Program.cs
+++ LessonMonth/LessonMonth/Program.cs
@@ -5,19 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LessonMonth
-{
 
-    
+{
+    enum Month : byte
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
 
 
     class Program
     {
-        
+
+
         static void Main(string[] args)
         {
 
-            
+            for (; ; )
+            {
+                Console.WriteLine("Введите название месяца (англ)");
+                foreach (string x in
+                     Enum.GetNames(typeof(Month))) Console.WriteLine(x);
+
+                try
+                {
+                    Month month = (Month)Enum.Parse(typeof(Month), Console.ReadLine(), true);
+                    if (!Enum.IsDefined(typeof(Month), month)) Console.WriteLine("Введите число от 1 до 12");
+                    else
+                    Console.WriteLine(month);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+
+                }
+                if (Console.ReadKey().KeyChar == 'z') break;
+
+            }
+
         }
+
     }
-    
+
 }
