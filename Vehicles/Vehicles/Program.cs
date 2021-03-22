6de0f603588c94039702f815d298950077dbe98d
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-03-22 21:49:55 +0300

    Наследование, классы, интерфейсы

---
+++ Vehicles/Vehicles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle bicycle = new Bicycle();
+            GroundVehicle bicycle2 = new Bicycle();
+            IMusculeVehicle bicycle3 = new Bicycle();
+            Bicycle bicycle4 = new Bicycle();
+        }
+    }
+}
---
+++ Vehicles/Vehicles/Vehicle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    abstract class Vehicle
+    {
+    }
+    abstract class GroundVehicle : Vehicle
+    {
+
+    }
+    abstract class SeaVehicle : Vehicle
+    {
+
+    }
+    abstract class AirVehicle : Vehicle
+    {
+
+    }
+    class Car : GroundVehicle, IMotorVehicle
+    {
+
+    }
+    class Bicycle : GroundVehicle, IMusculeVehicle
+    {
+
+    }
+    class MotorBoat : SeaVehicle, IMotorVehicle
+    {
+
+    }
+    class Boat : SeaVehicle, IMusculeVehicle
+    {
+
+    }
+    class Airplane : AirVehicle, IMotorVehicle
+    {
+
+    }
+    class Deltaplane: AirVehicle, IMusculeVehicle
+    {
+
+    }
+    interface IMotorVehicle
+    {
+
+    }
+    interface IMusculeVehicle
+    {
+
+    }
+}
