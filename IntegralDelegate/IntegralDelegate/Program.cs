c3f0205ebbed7ca0a275be8966a27c5789d4b16d
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-04-19 21:30:25 +0300

    Интеграл с делегатами

---
+++ IntegralDelegate/IntegralDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegralDelegate
+{
+    delegate double Y(double x);
+
+
+    class Program
+    {
+        static double Integral (Y y, double min, double max)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
