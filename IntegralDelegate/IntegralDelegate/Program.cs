b54e6aa1cf11b3da1e38c9776908d5437fc88f5b
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-05-17 21:30:43 +0300

    Делегат, интерфейс, стандартный делегат

--- IntegralDelegate/IntegralDelegate/Program.cs
+++ IntegralDelegate/IntegralDelegate/Program.cs
@@ -7,17 +7,63 @@ using System.Threading.Tasks;
 namespace IntegralDelegate
 {
     delegate double Y(double x);
+    
+    interface IFunction 
+    {
+        double Call (double x);
+    }
 
+    class Fuction : IFunction
+    {
+        public double Call(double x)
+        {
+            return 1;
+        }
+    }
 
     class Program
     {
         static double Integral (Y y, double min, double max)
         {
-            
+            double sum = 0;
+            for (double x = min; x < max; x += 1e-6)
+            {
+                sum = sum + (y(x) + y(x + 1e-6)) * 1e-6 / 2;
+            }
+            return sum;
         }
+        static double Integral (IFunction y, double min, double max)
+        {
+            double sum = 0;
+            for (double x = min; x < max; x += 1e-6)
+            {
+                sum = sum + (y.Call(x) + y.Call(x + 1e-6)) * 1e-6 / 2;
+            }
+            return sum;
+
+        }
+        static double StandartIntegral (Func<double, double> y, double min, double max)
+        {
+            double sum = 0;
+            for (double x = min; x < max; x += 1e-6)
+            {
+                sum = sum + (y(x) + y(x + 1e-6)) * 1e-6 / 2;
+            }
+            return sum;
+        }
+
+        static double Test (double x)
+        {
+            return 1;
+        }
+
         static void Main(string[] args)
         {
-
+            Console.WriteLine(Integral(Test, 0, 1));
+            Console.WriteLine(Integral(new Fuction(), 0, 1));
+            Console.WriteLine(StandartIntegral(Test, 0, 1));
+            Console.ReadKey();
+            //Задание на следующий урок: написать несколько методов для тестирования и написать метод хорд.
         }
     }
 }
