819f0b6c4c77ee4607de0297dc76e914c4b563b9
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-05-31 21:17:32 +0300

    Законченное упражнение на делегаты.

--- IntegralDelegate/Sorting/Program.cs
+++ IntegralDelegate/Sorting/Program.cs
@@ -12,8 +12,11 @@ namespace Sorting
         public int length;
         public int depth;
 
-        
-        
+        public override string ToString()
+        {
+            return name;
+        }
+
     }
     
     
@@ -24,15 +27,52 @@ namespace Sorting
         {
             return river1.name.CompareTo(river2.name);
         }
-        
+
+        static int CompareByLength(River river1, River river2)
+        {
+            return river1.length - river2.length;
+        }
+
+        static int CompareByDepth(River river1, River river2)
+        {
+            return river1.depth - river2.depth;
+        }
+
         static void Main(string[] args)
         {
 
-            
+            Console.WriteLine("Введите количество рек: ");
+            int riversNumber = int.Parse(Console.ReadLine());
+
 
             List<River> rivers = new List<River>();
+           
+
+            for(int x = 0; x<riversNumber; x++)
+            {
+                River river = new River();
+                Console.Write("Введите название реки: ");
+                river.name = Console.ReadLine();
+                Console.Write("Введите длину реки: ");
+                river.length = int.Parse(Console.ReadLine());
+                Console.Write("Введите глубину реки: ");
+                river.depth = int.Parse(Console.ReadLine());
+                rivers.Add(river);
+            }
 
             rivers.Sort(CompareByName);//Отсортировали список по имени реки.
+            rivers.ForEach(Console.WriteLine);
+            Console.WriteLine();
+            rivers.Sort(CompareByLength);
+            rivers.ForEach(Console.WriteLine);
+            Console.WriteLine();
+            rivers.Sort(CompareByDepth);
+            foreach(River x in rivers)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+
         
         }
     }
