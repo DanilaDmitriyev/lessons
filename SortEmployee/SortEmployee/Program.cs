557b62055ee8a486cf6a6763bb4da867b6be8508
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-06-07 20:35:13 +0300

    Домашнее задание на первую неделю июня

--- SortEmployee/SortEmployee/Program.cs
+++ SortEmployee/SortEmployee/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace SortEmployee
 {
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Skill { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    
+    
     class Program
     {
         static void Main(string[] args)
         {
+            //Создать список сотрудников, ввести в него данные, выполнить сортировки, вывести результат.
         }
     }
 }
