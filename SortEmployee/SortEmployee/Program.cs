a12e9e96d90070f171a565dfe5f145aee61e3e7b
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-06-07 21:32:16 +0300

    Делегаты при соротировке

--- SortEmployee/SortEmployee/Program.cs
+++ SortEmployee/SortEmployee/Program.cs
@@ -20,8 +20,29 @@ namespace SortEmployee
     
     class Program
     {
+        static int CompareNames (Employee x, Employee y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+
+        static int CompareNamesLength(Employee x, Employee y)
+        {
+            return x.Name.Length - y.Name.Length;
+        }
+
         static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(new Employee {Name = "Alex", Age = 20, Skill = 1 });
+            employees.Add(new Employee { Name = "Alexandra", Age = 50, Skill = 10 });
+            employees.Add(new Employee { Name = "Andre", Age = 19, Skill = 2 });
+
+            employees.Sort(CompareNames);
+            employees.Sort(CompareNamesLength);
+            employees.ForEach(Console.WriteLine);
+            Console.ReadKey();
+
             //Создать список сотрудников, ввести в него данные, выполнить сортировки, вывести результат.
         }
     }
