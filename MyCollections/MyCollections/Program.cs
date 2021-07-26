30ac3da02dbae981bd7113e55982cc43d19a3933
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-07-26 21:32:57 +0300

    Работа с коллекциями (недоделано). Разобраться с словарём.

--- MyCollections/MyCollections/Program.cs
+++ MyCollections/MyCollections/Program.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace MyCollections
 {
+    public enum Degree { candidate, doctor}
+
+
     
+    public class Employee
+    {
+        private Dictionary<string, int> discipline;
+        public string Family { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birth { get; set; }
+        public Degree degree { get; set; }
+        public Dictionary<string, int> Discipline
+        {
+            get { return discipline; }
+        }
+    }
     class Program
     {
 
@@ -14,6 +30,8 @@ namespace MyCollections
           список читаемых курсов (название, кол-во часов), список научных трудов (название, год издания, издательство),
           педагогическая нагрузка (общее кол-во часов по всем курсам). Для расчёта педагогической нагрузки использовать Linq*/
         //чем Enum в корне отличается от Коллекций
+
+        
 
 
         static void Main(string[] args)
