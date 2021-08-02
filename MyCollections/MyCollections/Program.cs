88586fb34c92ba14e928f10c7425e686873f8e68
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-08-02 21:36:55 +0300

    Работа с Linq

--- MyCollections/MyCollections/Program.cs
+++ MyCollections/MyCollections/Program.cs
@@ -7,21 +7,32 @@ using System.Threading.Tasks;
 namespace MyCollections
 {
     public enum Degree { candidate, doctor}
-
+    public class Publication
+    {
+        public string Article { get; set; }
+        public int Year { get; set; }
+        public string Publishier { get; set; }
+    }
 
     
     public class Employee
     {
-        private Dictionary<string, int> discipline;
+        private Dictionary<string, int> discipline = new Dictionary<string, int>();
         public string Family { get; set; }
         public string Name { get; set; }
-        public string Patronymic { get; set; }
+        public string Patronymic { get; set; }//отчество
         public DateTime Birth { get; set; }
         public Degree degree { get; set; }
         public Dictionary<string, int> Discipline
         {
             get { return discipline; }
         }
+        public List<Publication> Publications { get; } = new List<Publication>();
+        public int Throttle()
+        {
+            return discipline.Sum(kv => kv.Value); //расширяющий метод и лямбда выражение
+        }
+        
     }
     class Program
     {
@@ -30,12 +41,13 @@ namespace MyCollections
           список читаемых курсов (название, кол-во часов), список научных трудов (название, год издания, издательство),
           педагогическая нагрузка (общее кол-во часов по всем курсам). Для расчёта педагогической нагрузки использовать Linq*/
         //чем Enum в корне отличается от Коллекций
-
+        //почитать LINQ слайд 17, лекция 4 и слайд 16
         
 
 
         static void Main(string[] args)
         {
+
         }
     }
 }
