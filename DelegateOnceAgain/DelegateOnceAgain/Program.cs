95e8350955b7d4b3aac48ccdcd9ae95a8d613ad5
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-10-04 21:44:32 +0300

    Вновь делегат.

---
+++ DelegateOnceAgain/DelegateOnceAgain/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateOnceAgain
+{
+    public delegate void DelOA(bool x);
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            DelOA delOA = program.MyMetod;
+            delOA(true);
+            Console.ReadKey();
+        }
+        public void MyMetod (bool x)
+        {
+            if (x) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    }
+}
---
+++ DelegateOnceAgain/DelegateOnceAgain/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DelegateOnceAgain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("DelegateOnceAgain")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ee0cc7e2-d21f-4449-a67d-26b435ad5b9e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
