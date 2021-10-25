8602bdfb75e6bc941ba10df7336cc72751c45597
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-10-25 20:53:04 +0300

    Анонимные методы

--- Anonym/Anonym/Program.cs
+++ Anonym/Anonym/Program.cs
@@ -21,7 +21,7 @@ namespace Anonym
             Employer[] factory = new Employer[10];
             factory.Where(employer => employer.age > 30);//нажми F12
             factory.Where(employer_real);
-            factory.Where(delegate (Employer employer ) { return employer.age > 30; });
+            factory.Where(delegate (Employer employer ) { return employer.age > 30; });//Здесь анонимный метод
             factory.OrderBy(employer => employer.business);
             factory[0].age = 28;
             factory[0].EmpMetod(x => x * x);
