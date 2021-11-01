4e95c179dff807eec4f7b26b1267c3b8b3613aa1
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-11-01 21:33:32 +0300

    Только делегаты

---
+++ Anonym_once_again/Anonym_once_again/Program.cs
@@ -0,0 +1,32 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonym_once_again
+{
+    public delegate double MyFunction(double x, double y); 
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMaximum(0, 5, 0, 5, (x, y) => x * y + 10));
+            Console.ReadKey();
+        }
+        static double MyMaximum (double xmin, double xmax, double ymin, double ymax, MyFunction myFunction) {
+            double Max = double.MinValue;
+            for(double x=xmin; x<=xmax; x = x + 0.0001)
+            {
+                for (double y = ymin; y <= ymax; y = y + 0.0001)
+                {
+                    if (myFunction(x, y) > Max) Max = myFunction(x, y);
+                    
+                }
+            }
+            return Max;
+        }
+    }
+}
