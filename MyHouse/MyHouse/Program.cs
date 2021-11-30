41d665b37db9d3ccdf8a8b50801c593b04bf94fb
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-11-30 21:37:38 +0300

    Linq

---
+++ MyHouse/MyHouse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHouse
+{
+    class Material
+    {
+        private float size, weight;
+        private string material, color;
+        public float Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        public float Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public string MaterialType
+        {
+            get { return material; }
+            set { material = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Material[] materials = new Material [0];
+            materials.Where(m => m.Color == "red");
+            materials.OrderByDescending(m => m.Weight).First();
+        }
+    }
+}
