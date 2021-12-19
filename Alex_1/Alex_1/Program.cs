ae76db9be3a6bb2896aa5e27a5220314c6eeec93
Дрокончик босс Дрокончиков <9705151@gmail.com>
2021-12-19 20:28:40 +0300

    Интерфейсы

--- Alex_1/Alex_1/Program.cs
+++ Alex_1/Alex_1/Program.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace Alex_1
 {
+    interface IAboba
+    {
+        void Biba();
+        
+    }
+    class BaseAboba : IAboba
+    {
+        void IAboba.Biba() 
+        {
+            Console.WriteLine("Aboba");
+        }
+
+}
     class Program
     {
         static void Main(string[] args)
         {
+            IAboba Action2 = new BaseAboba();
+            Action2.Biba();
 
+            Console.ReadKey();
         }
     }
 }
