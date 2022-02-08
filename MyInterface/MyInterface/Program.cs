681fe6e2de70f4b7bb56e658a192dccadfbaa3c3
Дрокончик босс Дрокончиков <9705151@gmail.com>
2022-02-08 20:54:20 +0300

    Интерфейсы, 2 способа реализации

---
+++ MyInterface/MyInterface/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInterface
+{
+    public interface MainInterface
+    {
+        void PrintSomething(string x);
+    }
+    class Class1 : MainInterface
+    {
+        //явная реализация интерфейса
+        void MainInterface.PrintSomething(string x)
+        {
+            Console.WriteLine(x);
+        }
+    }
+    class Class2 : MainInterface
+    {
+       //реализация члена интерфейса, как члена класса
+        public void PrintSomething(string x)
+        {
+            Console.WriteLine(x);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 class2 = new Class2();
+            string temp = "text";
+            class2.PrintSomething(temp);
+
+            MainInterface class1 = new Class1(); //Переменной типа интерфейса присваиваем объект класса
+            class1.PrintSomething(temp);
+            Console.ReadKey();
+        }
+    }
+}
