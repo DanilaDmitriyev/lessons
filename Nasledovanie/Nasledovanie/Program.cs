54ef73284cbf3d73203836ded665fc1d2bde7a6e
Дрокончик босс Дрокончиков <9705151@gmail.com>
2022-03-01 21:06:59 +0300

    Класс, интерфейс, наследование

---
+++ Nasledovanie/Nasledovanie/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledovanie
+{
+    class Father
+    {
+        public void FPrint (string x) { Console.WriteLine(x); }
+    }
+
+    interface IMother
+    {
+        void MPrint(string x);
+    }
+
+    class Child : Father, IMother
+    {
+        void IMother.MPrint(string x) { Console.WriteLine(x); }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.FPrint("Класс");
+            IMother mother = new Child();
+            mother.MPrint("интерфейс");
+            Console.ReadKey();
+
+        }
+    }
+}
