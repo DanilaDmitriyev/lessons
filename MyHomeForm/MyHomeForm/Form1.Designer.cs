796c9250874cd19ab43a90c19bfa061323987381
Дрокончик босс Дрокончиков <9705151@gmail.com>
2022-03-20 18:37:13 +0300

    Самостоятельно Windows Forms

--- MyHomeForm/MyHomeForm/Form1.Designer.cs
+++ MyHomeForm/MyHomeForm/Form1.Designer.cs
@@ -29,13 +29,94 @@ namespace MyHomeForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.vatBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // priceBox
+            // 
+            this.priceBox.BackColor = System.Drawing.SystemColors.Info;
+            this.priceBox.Location = new System.Drawing.Point(40, 28);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(100, 26);
+            this.priceBox.TabIndex = 0;
+            // 
+            // vatBox
+            // 
+            this.vatBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.vatBox.Location = new System.Drawing.Point(177, 28);
+            this.vatBox.Name = "vatBox";
+            this.vatBox.Size = new System.Drawing.Size(100, 26);
+            this.vatBox.TabIndex = 1;
+            this.vatBox.Text = "20";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(306, 31);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(232, 35);
+            this.calcButton.TabIndex = 2;
+            this.calcButton.Text = "Рассчитать";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(568, 31);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(100, 26);
+            this.resultBox.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(40, 108);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(276, 56);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(40, 227);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(276, 56);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "Открыть";
+            this.openButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(716, 331);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.vatBox);
+            this.Controls.Add(this.priceBox);
+            this.MinimumSize = new System.Drawing.Size(738, 387);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.TextBox vatBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button openButton;
     }
 }
 
--- MyHomeForm/MyHomeForm/Form1.cs
+++ MyHomeForm/MyHomeForm/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace MyHomeForm
 {
     public partial class Form1 : Form
@@ -16,5 +18,22 @@ namespace MyHomeForm
         {
             InitializeComponent();
         }
+
+        public void calcButton_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(priceBox.Text);
+            int y = int.Parse(vatBox.Text);
+            float z = (x / (100 + y)) * y;
+            string zz = z.ToString();
+            //resultBox.Text.Insert(0, zz);
+            //resultBox.Paste(zz);
+          
+            
+
+        }
+        
+
+
+
     }
 }
