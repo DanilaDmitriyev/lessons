e40a6401867e898bd3997a34cd0c9d29d7c2b251
Дрокончик босс Дрокончиков <9705151@gmail.com>
2022-07-12 21:56:51 +0300

    Дата репозиторий

--- GMZ/GMZ/DataRepository.cs
+++ GMZ/GMZ/DataRepository.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace GMZ
 {
     class DataRepository
     {
-        //должен быть конфиг. файл, где указан путь к файлу данных
+        private string m_path;
+        public string Path
+        {
+            get
+            {
+                if (m_path != null) return m_path;
+                m_path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GMZ", "Data");
+                if (!Directory.Exists(m_path)) Directory.CreateDirectory(m_path);
+                return m_path;
+            }
+        }
+        public List<DataComponent> Components { get; } = new List<DataComponent>();
+        private static XmlSerializer xmlSerializer = new XmlSerializer(typeof(DataRepository));
+        //Создать метод, который будет возвращать DataRepository, прочитанный из файла.
     }
 }
--- GMZ/GMZ/Properties/Settings.Designer.cs
+++ GMZ/GMZ/Properties/Settings.Designer.cs
@@ -22,5 +22,17 @@ namespace GMZ.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string PATH {
+            get {
+                return ((string)(this["PATH"]));
+            }
+            set {
+                this["PATH"] = value;
+            }
+        }
     }
 }
