d02c1210e959a147fef86035d0bd9eec76869e2c
Дрокончик босс Дрокончиков <9705151@gmail.com>
2022-11-21 23:02:54 +0300

    Рисование спирали

---
+++ GeneratorSpirali/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GeneratorSpirali
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter streamWriter = new StreamWriter("spirali.html"))
+            {
+                //streamWriter.WriteLine(@"<svg width=""500"" height=""200"">" );
+                streamWriter.WriteLine("<svg width=\"500\" height=\"200\">");
+                streamWriter.WriteLine("</svg>");
+            }
+
+        }
+    }
+}
