5e88c8e676d3cbc23c39fcbf32acf7c25264bbed
Дрокончик босс Дрокончиков <9705151@gmail.com>
2022-12-01 19:02:06 +0300

    Векторная графика в HTML

--- GeneratorSpirali/Program.cs
+++ GeneratorSpirali/Program.cs
@@ -14,7 +14,10 @@ namespace GeneratorSpirali
             using (StreamWriter streamWriter = new StreamWriter("spirali.html"))
             {
                 //streamWriter.WriteLine(@"<svg width=""500"" height=""200"">" );
-                streamWriter.WriteLine("<svg width=\"500\" height=\"200\">");
+                streamWriter.WriteLine("<svg width=\"500\" height=\"202\">");
+                streamWriter.WriteLine("<polyline stroke=\"blue\" stroke-width=\"4px\" points=\"0,0, 500,200\"/>");
+                streamWriter.WriteLine("<polyline stroke=\"blue\" stroke-width=\"4px\" points=\"500,0, 0,200\"/>");
+                streamWriter.WriteLine("<path stroke=\"blue\" fill=\"transparent\" stroke-width=\"4px\" d=\"M 2 0 C 2 50, 2 200, 250 200\"/>");
                 streamWriter.WriteLine("</svg>");
             }
 
