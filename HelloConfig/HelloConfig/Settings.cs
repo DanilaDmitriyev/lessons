d1f21efd420a08e5e3d486a4752238277be3d6f7
daniladmitriyev <danila.dmitriyev@yandex.ru>
2024-01-28 23:19:28 +0300

    Первая выгрузка

---
+++ HelloConfig/HelloConfig/Settings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+
+namespace HelloConfig
+{
+    public class Settings
+    {
+        public IConfiguration config { get; set; }
+        public Settings(string rootPath)
+        {
+            var configurationBinder = new ConfigurationBuilder();
+            configurationBinder.SetBasePath(rootPath);
+            configurationBinder.AddJsonFile("appsettings.json");
+            config = configurationBinder.Build();
+           
+        } 
+            
+        
+    }
+}
